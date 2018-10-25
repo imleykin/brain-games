08faf6b1bde753a5a89e20a6806d6e5e838630ca
Ilya Leykin <imleykin@gmail.com>
2018-10-25 18:02:55 +0500

    add showWelcomeMessage function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
+const showWelcomeMessage = () => console.log('Welcome to the Brain Games! \n');
+
 const getUserName = () => {
   const name = readlineSync.question('May I have your name: ');
   console.log(`Hello, ${name}! \n`);
