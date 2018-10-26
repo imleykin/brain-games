9082b451ec77152c6b5c64cba517a66c94e2ec23
Ilya Leykin <imleykin@gmail.com>
2018-10-26 19:06:34 +0500

    update description of the game

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -4,7 +4,7 @@ const getQuestion = () => Math.floor(Math.random() * 100);
 const getCorrectAnswer = num => (num % 2 === 0 ? 'yes' : 'no');
 
 const evenGame = () => ({
-  description: 'Even game',
+  description: 'Answer "yes" if number even otherwise answer "no".',
   getQuiz: () => {
     const question = getQuestion();
     const answer = getCorrectAnswer(question);
