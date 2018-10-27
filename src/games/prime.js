158b1f6cee24ae26f7f9e950ae3c9a2ec81ea52e
Ilya Leykin <imleykin@gmail.com>
2018-10-28 01:02:20 +0500

    implement brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import startGame from '../startGame';
+import getRandomInteger from '../getRandomInteger';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const minForRandom = 0;
+const maxForRandom = 101;
+
+const isPrime = (num) => {
+  if (num === 0 || num === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+};
+
+const getGameRound = () => {
+  const question = getRandomInteger(minForRandom, maxForRandom);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return { question, answer };
+};
+
+export default () => startGame(gameDescription, getGameRound);
